e6f3160711dd842858eb905826a38ddd5ddf6935
dmmonir2003 <dmmonir2003@gmail.com>
2024-05-04 14:30:21 +0600

    prodect detailes created

--- src/app/(comonMainLayout)/products/[productId]/page.tsx
+++ src/app/(comonMainLayout)/products/[productId]/page.tsx
@@ -1,10 +1,76 @@
+"use client"
+
+import { Box, Breadcrumbs, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import ButtonBase from '@mui/material/ButtonBase';
+import Image from 'next/image';
 
 const ProductDetailPage = () => {
+
+
+
+
     return (
-        <div>
-            <h2>ProductDetailPage</h2>
-        </div>
+        <Container >
+            <Stack direction='row' alignItems='center'>
+                <Typography>Product Listing</Typography>
+                <ArrowForwardIosIcon fontSize='14px'></ArrowForwardIosIcon>
+                <Typography >Dummy Product Page</Typography>
+            </Stack >
+
+
+            <Box mt={5}>
+                <Paper
+                    sx={{
+                        p: 2,
+                        margin: 'auto',
+                        maxWidth: 'full',
+
+                    }}
+                >
+                    <Grid container spacing={2}>
+                        <Grid item sm={4}>
+                            <ButtonBase >
+                                <Image width={400} height={200} alt='prodect image' src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQz0CoRtQkrNS5X8fVWcVVyMyfgkbneCTVesp4oKhAF_lCaRMOb' ></Image>
+                            </ButtonBase>
+                        </Grid>
+                        <Grid item xs={12} sm={8} container>
+                            <Grid item xs container direction="column" spacing={2}>
+                                <Grid item xs>
+                                    <Typography gutterBottom variant="subtitle1" component="div">
+                                        Standard license
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        Full resolution 1920x1080 • JPEG
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        ID: 1030114
+                                    </Typography>
+                                    <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                                        Remove
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+
+                                </Grid>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="subtitle1" component="div">
+                                    $19.00
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
+
+
+        </Container>
     );
 };
 
--- src/components/home/FlashCard.tsx
+++ src/components/home/FlashCard.tsx
@@ -1,53 +1,53 @@
+
+
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box, Grid, Icon, Stack } from '@mui/material';
+import { Box, Checkbox, Grid, Icon, Stack } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import CardHover from '../clientCompo.tsx/CardHover';
+import Link from 'next/link';
 
 const FlashCard = ({ data }) => {
     console.log(data);
+
+
+
     return (
         <>
-            <Grid container spacing={{ sm: 2, sx: 1, md: 2 }} columns={{ xs: 1, sm: 2, md: 4 }} justifyContent="center"
+            <Grid container gap={2} spacing={{ sm: 2, sx: 1, md: 2 }} columns={{ xs: 1, sm: 2, md: 4 }} justifyContent="center"
                 alignItems="center">
                 {data.map((products, index) => (
                     <Grid item key={index}>
-                        <Card sx={{ maxWidth: 255, minWidth: 255, minHeight: 360, maxHeight: 360, position: 'relative', }}>
+                        <Card sx={{ maxWidth: 255, minWidth: 255, minHeight: 380, maxHeight: 380, position: 'relative', p: 1 }}>
                             <IconButton aria-label="percentage" sx={{
                                 position: 'absolute', top: 0, left: 0, backgroundColor: red[500], '&:hover': {
                                     backgroundColor: red[500],
-                                }, borderRadius: '', padding: '2px', px: 2, textAlign: 'center', ml: 1, mt: 1
+                                }, padding: '2px', px: 2, textAlign: 'center', ml: 1, mt: 1,
                             }}>
                                 <Typography component={'p'} sx={{ fontSize: 12, color: 'white' }} > -13%</Typography>
                             </IconButton>
-                            <IconButton aria-label="add to favorites" sx={{ position: 'absolute', top: -2, right: 0, zIndex: 3, color: red[500] }}>
-                                <FavoriteIcon sx={{ fontSize: 24, }} />
+                            <IconButton aria-label="add to favorites" sx={{ position: 'absolute', top: -7, right: 0, zIndex: 3, }}>
+
+                                <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: red[500] }} />} />
                             </IconButton>
 
                             <CardMedia
                                 component="img"
                                 height=""
+
                                 image={products?.imagesUrl}
                                 alt="card image"
-                                sx={{ maxHeight: 250, borderRadius: 2, minHeight: 250, padding: 4, }}
+                                sx={{ maxHeight: 250, borderRadius: 2, minHeight: 250, pt: 4, px: 4, }}
 
                             />
-                            <CardContent sx={{ mb: 0 }}>
-                                <Stack direction='row' alignItems='end' justifyContent='space-between'>
-
+                            <CardContent sx={{ mb: 3, position: 'relative' }}>
+                                <Stack direction='row' alignItems='end' justifyContent='space-between' sx={{}}>
 
                                     <Box>
                                         <Typography color="text.secondary">
@@ -61,6 +61,18 @@ const FlashCard = ({ data }) => {
                                     <Box>
                                         <AddCircleOutlineIcon></AddCircleOutlineIcon>
                                     </Box>
+                                    <IconButton
+                                        aria-label="show details"
+                                        sx={{
+                                            position: 'absolute',
+                                            bottom: -22,
+                                            right: 40,
+                                            zIndex: 1,
+
+                                        }}
+                                    >
+                                        <Link href='/products/1'>Show Details</Link>
+                                    </IconButton>
                                 </Stack>
 
 
@@ -71,9 +83,14 @@ const FlashCard = ({ data }) => {
                         </Card>
                     </Grid>
                 ))}
-            </Grid>
+            </Grid >
         </>
     );
 };
 
 export default FlashCard;
+
+// sx={{ maxWidth: 255, minWidth: 255, minHeight: 360, maxHeight: 360, position: 'relative', }}
+
+
+
