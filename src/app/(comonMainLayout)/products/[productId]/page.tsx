4c64b2ece3e5978eb347000a6667e2c7825a9421
dmmonir2003 <dmmonir2003@gmail.com>
2024-05-05 13:26:32 +0600

    prodect detailes image add

--- src/app/(comonMainLayout)/products/[productId]/page.tsx
+++ src/app/(comonMainLayout)/products/[productId]/page.tsx
@@ -1,6 +1,6 @@
 
 
-import { Box, Breadcrumbs, Container, Stack, Typography } from '@mui/material';
+import { Box, Breadcrumbs, Checkbox, Container, Divider, IconButton, Rating, Stack, Typography } from '@mui/material';
 import React from 'react';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -10,7 +10,9 @@ import Paper from '@mui/material/Paper';
 import ButtonBase from '@mui/material/ButtonBase';
 import Image from 'next/image';
 import { green, red } from '@mui/material/colors';
-
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import ElectricRickshawIcon from '@mui/icons-material/ElectricRickshaw';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 const ProductDetailPage = () => {
 
 
@@ -47,7 +49,46 @@ const ProductDetailPage = () => {
                         </Stack>
                     </Grid>
                     <Grid item xs={12} sm={6} sx={{ bg: green[500] }}>
-                        <Typography>text</Typography>
+                        <Stack direction='row' justifyContent='space-between'>
+                            <Box
+
+                            >
+                                <Typography fontSize={30} fontWeight={700}>Double Bed & Side Tables</Typography>
+
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        color: 'black',
+                                        gap: 1,
+
+                                    }}
+                                >
+                                    <Typography fontSize={20}>$54.98</Typography>
+                                    <Divider color='black' orientation="vertical" variant="middle" flexItem sx={{ height: 15, width: 2 }} />
+                                    <Rating name="half-rating" defaultValue={2.5} precision={0.5} /> <Typography component={'span'}>( 32 review )</Typography>
+                                </Box>
+                                <Divider sx={{ my: 2, }} />
+
+                            </Box>
+                            <Box>  <IconButton aria-label="add to favorites">
+                                <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: red[500] }} />} />
+                            </IconButton>
+                            </Box>
+
+
+                        </Stack>
+                        <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, amet! Quam maxime eius accusantium beatae facere tenetur nesciunt illo earum. Quam maxime eius accusantium beatae facere tenetur nesciunt illo earum.</Typography>
+
+                        <Box my={4} ml={1}>
+                            <Typography component={'li'}>Lorem ipsum dolor sit amet, adipi scing elit</Typography>
+                            <Typography component={'li'}>Lorem ipsum dolor sit amet, adipi scing elit</Typography>
+                            <Typography component={'li'}>Lorem ipsum dolor sit amet, adipi scing elit</Typography>
+
+                        </Box>
+
+                        <Stack direction='row' gap={2} alignItems='center'> <LocalShippingIcon></LocalShippingIcon> <Typography>Free worldwide shipping on all orders over $100</Typography></Stack>
+
                     </Grid>
 
                 </Grid>
